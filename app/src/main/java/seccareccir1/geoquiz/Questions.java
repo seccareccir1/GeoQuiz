8dd6f12ee19f7e868f394a47f8b5b29fd5981b0d
Robert Seccareccia <robert.seccareccia.jr@gmail.com>
2017-09-20 12:35:19 -0400

    Updated files for project 2

---
+++ app/src/main/java/seccareccir1/geoquiz/Questions.java
@@ -0,0 +1,17 @@
+package seccareccir1.geoquiz;
+
+/**
+ * Created by RobertSeccareccia on 9/18/17.
+ */
+
+public class Questions {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Questions(int textResId, boolean answerTrue){
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+}
